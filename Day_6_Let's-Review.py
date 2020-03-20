b592df5a4abe23db12327c00807bc1f46613e9d1
ahmadbayhaqi <56214296+Kyuubang@users.noreply.github.com>
2020-03-20 23:21:10 +0700

    Update Day_6_Let's-Review.py

--- Day_6_Let's-Review.py
+++ Day_6_Let's-Review.py
@@ -1,3 +1,14 @@
-print ("hello")
-var = input("enter your name :")
-print("hello "+var,"your run this script in atom editor")
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+int_test = int(input())
+
+for i in range(int_test):
+    test = input()
+    odd = []
+    even = []
+    for x in range(len(test)):
+        if x % 2  == 0 :
+            even.append(test[x])
+        else :
+            odd.append(test[x])
+    print("".join(even), "".join(odd))
