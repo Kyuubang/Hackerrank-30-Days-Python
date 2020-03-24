7b9d6a11dd60fc5130d745a24a5fb9fc039b03b4
ahmadbayhaqi <56214296+Kyuubang@users.noreply.github.com>
2020-03-25 05:44:00 +0700

    Create Day_11_2D-Array.py

---
+++ Day_11_2D-Array.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def maxSum(arr):
+    print(arr)
+
+if __name__ == '__main__':
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    maxSum(arr)
