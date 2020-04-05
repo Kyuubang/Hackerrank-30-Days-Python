06c156200d101b5e8082d831bacfd474a395522b
Kyuubang <bayhaqi0404ahmad@gmail.com>
2020-04-05 23:43:24 +0700

    add day 17 "More Exceptions"

---
+++ Day 17/solution_1.py
@@ -0,0 +1,14 @@
+#Write your code here
+class Calculator:
+    def power(n,p):
+
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
