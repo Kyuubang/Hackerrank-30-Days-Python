cbda4e9e08f510f807f336a40b19413ff8ee7da8
Kyuubang <bayhaqisptr04@gmail.com>
2020-04-08 12:59:32 +0700

    add day 25 solution

--- Day 25/solution_1.py
+++ Day 25/solution_1.py
@@ -9,12 +9,12 @@ def isPrime(n):
 
     if n <= 1:
         return False
-    for i in range(2, n):
+    for i in range(2, int(n**0.5+1)):
         if n % i == 0:
             return False
-    
+
     return True
-    
+
 for i in (List):
     x = isPrime(i)
     if x:
