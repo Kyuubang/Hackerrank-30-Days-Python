e9f0c468dac8b816220fc1683605a3c3146f1492
Kyuubang <bayhaqisptr04@gmail.com>
2020-04-08 13:00:20 +0700

    modify

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-s = input()
-
-lenght = len(s)
-
-for i in range(lenght):
-    print(s[i])
+x = input()
+y = input()
+x = x.split()
+y = y.split()
+year = x.pop()
+years = y.pop()
+x.pop()
+y.pop()
+print(x)
+print(y)
+print(year)
+print(years)
+# if year > years:
+#     print(10000)
+# else :
+#     hackos = [15,500]
+#     for i in ((range(2))[::-1]):
+#         if int(x[1 2i]) > int(y[i]):
+#             print(int(x[i])-int(y[i]))*(hackos[i])
+#             break
+#         print(0)
+#         break
